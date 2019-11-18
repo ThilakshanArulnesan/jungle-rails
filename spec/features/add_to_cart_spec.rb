c4cac23a8776c57825c07717f2ea675b0dc8f9c5
Thilakshan <th_thilakshan@hotmail.com>
2019-11-18 17:32:51 -0500

    add test for adding to cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    visit root_path
+    expect(page).to have_css '.fa-shopping-cart'
+    # visit (first 'de').click
+    expect(first('a[href="/cart"]').text).to include("0")
+
+    (first 'button').click
+    expect(first('a[href="/cart"]').text).to include("1")
+
+    # expect(page).to have_css('article.product-detail')
+  end
 end
